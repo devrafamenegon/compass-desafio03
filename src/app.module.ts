7c68bf3645408afcb9ddf3abadc02e99f90b35f4
devrafamenegon <dev.rafaelmenegon@gmail.com>
2022-07-29 19:59:44 -0300

    :wrench: chore: TypeORM config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,32 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { DataSource } from 'typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { EmployeeModule } from './employee/employee.module';
 
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+
 @Module({
-  imports: [EmployeeModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: 'maries_company',
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+    EmployeeModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
