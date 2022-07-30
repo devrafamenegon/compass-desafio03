e347432288c63198d1bc9a896bb7f4a6a89f88d3
devrafamenegon <dev.rafaelmenegon@gmail.com>
2022-07-30 20:11:19 -0300

    :wrench: fix: Repository and product module import

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -2,12 +2,13 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { ProductController } from "./controllers/product.controller";
 import { Product } from "./entities/product.entity";
+import { ProductRepository } from "./repositories/product.repository";
 import { ProductService } from "./services/product.service";
 
 @Module({
   imports: [TypeOrmModule.forFeature([Product])],
   controllers: [ProductController],
-  providers: [ProductService, ProductService],
+  providers: [ProductService, ProductRepository],
 })
 
 export class ProductModule {}
